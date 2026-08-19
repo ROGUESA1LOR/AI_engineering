71d60b5c2a409fb3445eee8defbb339156a9995b
Hassan <roguesailor72@gmail.com>
2026-08-19 16:19:45 +0500

    testing dev branch

--- podman-agentic-ci-prototype/broken.py
+++ podman-agentic-ci-prototype/broken.py
@@ -3,4 +3,5 @@ import sys
 print("Initializing system code blocks...")
 
 # Deliberate intentional crash: calling a function name that does not exist!
-execute_non_existent_backend_logic_lol()
+execute_non_existent_backend_logic_lol():
+print("hello")
